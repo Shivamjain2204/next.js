4a5c632669280226c66e3f742cf09306a3b64277
Shivam <jainshivam2204@gmail.com>
2024-03-10 09:18:54 +0530

    navbar updated

--- my-app/components/Navbar.jsx
+++ my-app/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Navbar() {
   return (
-    <div>
-      Navbar Page
-    </div>
+    <nav className="flex justify-between px-4 bg-slate-800 text-white py-4">
+      <div className="logo font-bold">App</div>
+      <ul className="flex gap-6">
+        <Link href="/"><li>Home</li></Link>
+        <Link href="/about"><li>About</li></Link>
+        <Link href="/contact"><li>Contact</li></Link>
+      </ul>
+    </nav>
   )
 }
 
